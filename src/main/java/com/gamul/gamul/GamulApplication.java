0e5cf9797ad50856046f649d838fd23fd41fff8d
이지영 <wldudwldhd@naver.com>
2022-10-20 15:04:16 +0900

    fist commit

---
+++ src/main/java/com/gamul/gamul/GamulApplication.java
@@ -0,0 +1,13 @@
+package com.gamul.gamul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamul/gamul/GamulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamul.gamul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
