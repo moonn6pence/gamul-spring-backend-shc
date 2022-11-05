fc806fe8330a123bca35b8aa1217008f5809cb65
moonn6pence <shin421179@gmail.com>
2022-11-05 15:24:33 +0900

    fix : 엔티티 수정
    - UserLocation -> Bookmark
    - Bookmark에서 x,y 필드 삭제하고 Market엔티티와 맵핑
    - Product 삭제
    - MarketProductHistory -> PriceHistory
    - PriceHistory에 name(상품명)필드,unit 필드 추가
    - District 삭제
    - Market에서 district 필드 삭제

--- src/main/java/com/gamul/gamul/domain/entity/PriceHistory.java
+++ src/main/java/com/gamul/gamul/domain/entity/PriceHistory.java
@@ -6,19 +6,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Getter @Setter
 @NoArgsConstructor
-public class UserLocation {
+@Getter @Setter
+public class PriceHistory {
     @Id @GeneratedValue
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="userId")
-    private User user;
+    private String name;
 
-    private Float x;
-    private Float y;
+    private LocalDateTime date;
+
+    private Long price;
+
+    private String unit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "marketId")
+    private Market market;
+
+
 }
